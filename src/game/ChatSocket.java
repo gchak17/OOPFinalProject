462cbc8819b24f124be404248c1e45d09f78d162
lilu-ps <izibaghdavadze@gmail.com>
2019-07-05 16:07:13 +0400

    # WARNING: head commit changed in the meantime
    chat socket

---
+++ src/game/ChatSocket.java
@@ -0,0 +1,58 @@
+package game;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+
+import javax.servlet.http.HttpSession;
+import javax.websocket.EncodeException;
+import javax.websocket.EndpointConfig;
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.ServerEndpoint;
+
+import org.json.JSONObject;
+
+import message.Message;
+import message.MessageDecoder;
+import message.MessageEncoder;
+
+@ServerEndpoint(value = "/chatSocket", encoders = { MessageEncoder.class }, decoders = { MessageDecoder.class })
+public class ChatSocket {
+
+	public static List<Session> sessionList = Collections.synchronizedList(new ArrayList<Session>());
+	private static ReentrantLock chatlock = new ReentrantLock();
+
+	@OnOpen
+	public void onOpen(Session session, EndpointConfig config) {
+		HttpSession httpSession = (HttpSession) config.getUserProperties().get(HttpSession.class.getName());
+		String username = (String) httpSession.getAttribute("username");
+		session.getUserProperties().put("username", username);
+		sessionList.add(session);
+	}
+
+	@OnClose
+	public void onClose(Session session) {
+		sessionList.remove(session);
+	}
+
+	@OnMessage
+	public void sendMessage(String message, Session session) throws IOException, EncodeException {
+		if (message == "") return;
+		String username = (String) session.getUserProperties().get("username");
+		chatlock.lock();
+		for (Session curSes : sessionList) {
+            if (!curSes.equals(session)) {
+            	JSONObject js = new JSONObject();
+            	js.append("username", username);
+            	js.append("message", message);
+            	curSes.getBasicRemote().sendText(js.toString());
+            }
+        }
+		chatlock.unlock();
+	}
+}
