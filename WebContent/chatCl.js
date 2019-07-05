72a070d92351a6273706258063ba0e7ae1426fd9
lilu-ps <izibaghdavadze@gmail.com>
2019-07-05 19:28:45 +0400

    not done

--- WebContent/chatCl.js
+++ WebContent/chatCl.js
@@ -5,7 +5,7 @@ var chatSocket = new WebSocket(wsUri2);
 chatSocket.onmessage = function(evt) { onChatMessage(evt) };
 
 function onChatMessage(evt){
-	alert("not that dumb");
+	alert("kill me");
 	var json = JSON.parse(evt.data);
 	$('#chatBox').append("<p class='username'>"+json.username+"</p><p class='text'>"+":"+json.message+"</p><br/>");
 	$('#chatBox').scrollTop($('#chatBox')[0].scrollHeight);
