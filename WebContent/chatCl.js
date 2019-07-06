5a967822c79274c98666e2c9df8dd035d65c126c
lilu-ps <izibaghdavadze@gmail.com>
2019-07-06 18:06:34 +0400

    chat not scrollable

--- WebContent/chatCl.js
+++ WebContent/chatCl.js
@@ -6,20 +6,11 @@ chatSocket.onmessage = function(evt) {
 };
 
 function onChatMessage(evt) {
-	alert("kill me");
 	var json = JSON.parse(evt.data);
-	alert(json.username + ": " + json.message);
-	alert(document.getElementById("chatBox"));
-	document.getElementById("chatBox").append("<p>" + json.username + ": "+json.message+"</p>");
-	//$('#chatBox').scrollTop($('#chatBox')[0].scrollHeight);
-		//	"<p class='username'>" + json.username + "</p><p class='text'>"
-		//			+ ":" + json.message + "</p><br/>");
-	alert("holy forking shirtballs");
-	alert("holy forking shirtballs");
-	alert("holy forking shirtballs");
+	document.getElementById("chatBox").append("text\n");
+	document.getElementById("chatText").value = "";
 }
 
 function clickOnSend() {
-	alert("here");
 	chatSocket.send(document.getElementById("chatText").value);
 }
