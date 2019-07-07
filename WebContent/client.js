7fa3b2f76ef388875e320655579ab880f435c750
lilu-ps <izibaghdavadze@gmail.com>
2019-07-07 16:14:17 +0400

    chat listens to client.js

--- WebContent/client.js
+++ WebContent/client.js
@@ -1,10 +1,30 @@
 //var wsUri = "ws://" + document.location.host + document.location.pathname + "websocket";
-
+var chatSocket = new WebSocket(
+		"ws://localhost:8081/OOPFinalProject/EnterChatServlet");
 
 var websocket = new WebSocket("ws://localhost:8080/OOPFinalProject/client.html");
 websocket.onmessage = function(evt) {
 	onMessage(evt)
 };
+
+chatSocket.onmessage = function(evt){
+	onChatMessage(evt)
+};
+
+function onChatMessage(evt) {
+	var json = JSON.parse(evt.data);
+	var node = document.createElement("P");
+
+	var textnode = document.createTextNode(json.username + ": " + json.message);
+	node.appendChild(textnode);
+
+	document.getElementById("chatBox").appendChild(node);
+	document.getElementById("chatText").value = "";
+}
+
+function clickOnSend() {
+	chatSocket.send(document.getElementById("chatText").value);
+}
 
 function sendText(json) {
 	websocket.send(json);
