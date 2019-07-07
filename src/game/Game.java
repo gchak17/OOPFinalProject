3f61523c864e6c31fa1ea05373a6fa01b5e4b6e9
anoddos <48618560+anoddos@users.noreply.github.com>
2019-07-07 17:38:51 +0400

    changed class connections and it will be shotasRomMoewonebaIseti code

--- src/game/Game.java
+++ src/game/Game.java
@@ -1,43 +1,108 @@
 package game;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Timer;
+
+import dao.Account;
 
 public class Game {
 	private ArrayList<Player> players;
-	private Player artist, winner;
+	private HashMap<Player, Integer> points;
+
+	private Player artist;
 	private String id;
 	private int maxPlayers;
 	private int time;
 	private int numRounds;
 	private int painterIndex;
-	
+	private Timer betweenRoundsTimer;
+	private boolean everybodyGuessed;
+
 	private boolean isGameOver = false;
-	
+	private boolean isRoundOver = false;
+	private Round round;
+	private int nthRound;
+
 	public Game(ArrayList<Player> players, int round, int time, String id) {
-		this.players = players;			
+		this.players = players;
 		this.numRounds = round;
 		this.time = time;
 		this.id = id;
 		painterIndex = 0;
-		choosePainter();
+		nthRound = 0;
+		startNewRound();
 	}
-	
+
 	public String getId() {
 		return this.id;
 	}
-	
+
+	public Round getRound() {
+		return round;
+	}
+
 	private void choosePainter() {
-		while(true) {
-			if(players.get(painterIndex).isInGame()) {
-				players.get(painterIndex).startDrawing();
+		while (true) {
+			if (players.get(painterIndex).isInGame()) {
+				artist = players.get(painterIndex);
 				painterIndex++;
 				break;
 			}
 			painterIndex++;
 		}
 	}
-	
 
-	
-	
+	public void startNewRound() {
+		choosePainter();
+		nthRound++;
+		Round r = new Round(nthRound, players, artist);
+		this.round = r;
+	}
+
+	public void endRound() {
+		this.round.endRound();
+		addRoundResToTotalScores();
+
+		if (nthRound == numRounds) {
+			endGame();
+		}
+	}
+
+	private void addRoundResToTotalScores() {
+		HashMap<Player, Integer> roundPoints = round.getPoints();
+		for (Player key : points.keySet()) {
+			int prevRes = points.get(key);
+			int res = roundPoints.get(key) + prevRes;
+			points.put(key, res);
+		}
+	}
+
+	private void endGame() {
+
+		showResults();
+
+		ArrayList<Player> leftPlayers = playersWantToPlayAgain();
+		if (leftPlayers.size() > 0) {
+			// start game from 0
+			this.players = leftPlayers;
+			points = new HashMap<Player, Integer>();
+			painterIndex = 0;
+			nthRound = 0;
+			choosePainter();
+			startNewRound();
+		}
+	}
+
+	private void showResults() {
+
+	}
+
+	private ArrayList<Player> playersWantToPlayAgain() {
+		// ask if they want to play again
+
+		return null;
+	}
+
 }
--- src/game/GameEndpoint.java
+++ src/game/GameEndpoint.java
@@ -14,43 +14,44 @@ import org.json.JSONObject;
 import dao.Account;
 import message.*;
 
-@ServerEndpoint(value = "/client.html", encoders = {MessageEncoder.class}, decoders = {MessageDecoder.class}, configurator = GameSocketConfig.class)
+@ServerEndpoint(value = "/client.html", encoders = { MessageEncoder.class }, decoders = {
+		MessageDecoder.class }, configurator = GameSocketConfig.class)
 public class GameEndpoint {
-    public static List<Session> peers = Collections.synchronizedList(new ArrayList<Session>());
-    
-    @OnOpen
-    public void onOpen(Session peer, EndpointConfig config) {
+	public static List<Session> peers = Collections.synchronizedList(new ArrayList<Session>());
 
-    	peers.add(peer);
-    }
+	@OnOpen
+	public void onOpen(Session peer, EndpointConfig config) {
 
-    @OnClose
-    public void onClose(Session peer) {
-        peers.remove(peer);
-    }
+		peers.add(peer);
+	}
 
-    @OnMessage
-    public void sendMessage(Message message, Session session) throws IOException, EncodeException {
-    	if(message.getJson().getString("type").equals("isArtist?") ) {    	
-        	JSONObject json = message.getJson();
-        	json.put("answer", false);
+	@OnClose
+	public void onClose(Session peer) {
+		peers.remove(peer);
+	}
 
-            HttpSession httpSession = ((HttpSession)session.getUserProperties().get("HttpSession"));
-            Player user = (Player) httpSession.getAttribute("player");
-            //System.out.println(user);
-            
-        	if(user.isArtist()) {
-        		json.put("answer", true);
-        	}
-        	
-        	message.setJson(json);
-        	session.getBasicRemote().sendObject(message);
-        }else {
-	    	for (Session peer : peers) {
-	            if (!peer.equals(session)) {
-	                peer.getBasicRemote().sendObject(message);
-	            }
-	        }
-        }
-    }
+	@OnMessage
+	public void sendMessage(Message message, Session session) throws IOException, EncodeException {
+		if (message.getJson().getString("type").equals("isArtist?")) {
+			JSONObject json = message.getJson();
+			json.put("answer", false);
+
+			HttpSession httpSession = ((HttpSession) session.getUserProperties().get("HttpSession"));
+			Player user = (Player) httpSession.getAttribute("player");
+			// System.out.println(user);
+
+			if (user.isArtist()) {
+				json.put("answer", true);
+			}
+
+			message.setJson(json);
+			session.getBasicRemote().sendObject(message);
+		} else {
+			for (Session peer : peers) {
+				if (!peer.equals(session)) {
+					peer.getBasicRemote().sendObject(message);
+				}
+			}
+		}
+	}
 }
