98b07e8b0ca58f31b177f00bcbe4854eb2eec7cb
lilu-ps <48617956+lilu-ps@users.noreply.github.com>
2019-07-13 21:32:27 +0400

    artist can exit!!!!!! <3

--- src/game/Game.java
+++ src/game/Game.java
@@ -194,7 +194,7 @@ public class Game {
 			// this.date = new Date(System.currentTimeMillis());
 			this.turnCounter = 0;
 			initMapRound();
-			roundStarterArtist = players.get(0);
+			//roundStarterArtist = players.get(0);
 			startTurn();
 		}
 		
@@ -257,6 +257,7 @@ public class Game {
 				
 				endTurnTimer.cancel();
 				players.remove(p);
+				//if (p.equals(roundStarterArtist)) roundStarterArtist = players.get(0);
 				endTurn();
 			} else {
 				players.remove(p);
@@ -282,11 +283,13 @@ public class Game {
 
 					artist = players.get(index);
 					if (artist.equals(roundStarterArtist)) {
+						System.out.println("fooork");
 						artist.shouldBeArtist(false);
 						turnOffTimers();
 						endRound();
 						roundIsnotEnded = false;
 					}
+					roundStarterArtist = players.get(0);
 				}
 			}
 		}
