3ccc6ea0368cc43e4255c993bb10d258a2a246f8
lilu-ps <48617956+lilu-ps@users.noreply.github.com>
2019-07-15 12:05:59 +0400

    message works

--- WebContent/notificationSocket.js
+++ WebContent/notificationSocket.js
@@ -1,6 +1,6 @@
-var notificationSocket = new WebSocket("ws://192.168.98.14:8888/OOPFinalProject/notifications");
+//var notificationSocket = new WebSocket("ws://192.168.98.14:8888/OOPFinalProject/notifications");
 //var notificationSocket = new WebSocket("ws://localhost:8888/OOPFinalProject/notifications");
-//var notificationSocket = new WebSocket('ws://' + window.location.host + '/OOPFinalProject/notifications');
+var notificationSocket = new WebSocket('ws://' + window.location.host + '/OOPFinalProject/notifications');
 		
 notificationSocket.onmessage = function(evt){
 	var notification = JSON.parse(evt.data);
